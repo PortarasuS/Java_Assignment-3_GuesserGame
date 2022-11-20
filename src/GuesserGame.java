2a14a58c5b7a26bdf79cddd76630898bfc10b71a
PortarasuS <111957586+PortarasuS@users.noreply.github.com>
2022-11-20 17:48:58 +0530

    First Version Guesser Game

--- src/GuesserGame.java
+++ src/GuesserGame.java
@@ -1,18 +1,16 @@
 import java.util.*;
 import java.lang.Math;   
 
-
 class Guesser{
 	private int guesserValue;
-	
-	
+
+
 	void setGuesserValue (int guesserValue) {
 		this.guesserValue = guesserValue;
 	}
 	int getGuesserValue() {
 		return this.guesserValue;
 	}
-	
 }
 
 class Player{
@@ -25,61 +23,68 @@ class Player{
 	public void setPlayerValue(int playerValue) {
 		this.playerValue = playerValue;
 	}
-	
-	
 }
 
 class Umpire{
 	int gusserInp = 0;
 	int playerInp;
+	int count = 0;
 	void eveluate (int gusserInp, int playerInp){
 		this.gusserInp = gusserInp;
 		this.playerInp = playerInp;
 		if (this.gusserInp == this.playerInp)
 		{	
+			count++;
 			System.out.println();
-			System.out.println("congratz! Player, You won the match");
+			System.out.println("congratz! Player - " + count +" , You won the match");
 		}
 		else if (this.gusserInp > this.playerInp){
+			count++;
 			System.out.println();
-			System.out.println("Player selected lesser value");
+			System.out.println("Player - "+count +" selected lesser value");
 		}
 		else if (this.gusserInp < this.playerInp){
+			count ++;
 			System.out.println();
-			System.out.println("Player selected greater value");
+			System.out.println("Player - "+ count +" selected greater value");
 		}
-		
-		
 	}
 }	
 
 public class GuesserGame {
 
 	public static void main(String[] args) {
-		
-		//Guesser Object
+
+		Scanner scan0 =new Scanner(System.in);
+		System.out.println("Please eneter the NUMBER OF PLAYERS participating in this game : ");
+		int playercnt = scan0.nextInt();
+
 		int randomval = (int)(Math.random()*10);
 		Guesser gsr = new Guesser();
-		//Scanner scan = new Scanner (System.in);
-		System.out.println("Hi, Player -- Guesser selected the value which is hidden , Please select player value (0 to 9) " );
-		// int guesserValue = scan.nextInt();
+		System.out.println("Hi, Player -- GUESSER selected the value which is HIDDEN , Player, Please select player value (0 to 9) to find the guesser value" );
+		System.out.println();
 		gsr.setGuesserValue(randomval);
 		int gsrOut = gsr.getGuesserValue();
-		
-		// Player 1 object
-		Player plr1 = new Player();
-		Scanner scan2 = new Scanner (System.in);
-		System.out.println("Please enter the Player-1 value : ");
-		plr1.setPlayerValue(scan2.nextInt());
-		int playerValue = plr1.getPlayerValue();
-		System.out.println(playerValue);
-		System.out.println();
-		System.out.println("Please find the Guesser value : " + gsrOut);
-		
-		Umpire ump = new Umpire();
-		ump.eveluate(randomval, playerValue);
-		
-	}
-	
 
+
+		int [] playerValue = new int[playercnt];
+		for (int i=0; i<playercnt; i++)
+		{
+
+			Player plr1 = new Player();
+			Scanner scan2 = new Scanner (System.in);
+			int count = i+1;
+			System.out.println("Please enter the Player - " +count +" value : ");
+			plr1.setPlayerValue(scan2.nextInt());
+			playerValue[i] = plr1.getPlayerValue();
+			System.out.println("VALUE OF PLAYER  "+count +" is "+ playerValue [i]);
+			System.out.println();
+		}
+		System.out.println("Please find the GUESSER VALUE : " + gsrOut);
+		Umpire ump = new Umpire();
+		for (int a : playerValue)
+		{
+			ump.eveluate(randomval, a);
+		}
+	}
 }
